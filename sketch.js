055e997bad4a86025747c38391536f417ffa091a
180608ar <73707937+180608ar@users.noreply.github.com>
2020-12-17 21:03:32 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -11,16 +11,23 @@ function setup(){
   myWorld = myEngine.world;
 
   ground = new Ground(600,590,1200,20);
+
   box1 = new Box(800,550);
   box2 = new Box(1000,550); 
+  pig1 = new Pig(900,550);
+
+  bird = new Bird(100,100);
 }
 
 function draw(){
+  background(0);
   Engine.update(myEngine);
 
   box1.display();
   box2.display();
   ground.display();
+  pig1.display();
+  bird.display();
 }
 
 
