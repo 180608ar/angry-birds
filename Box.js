0f7957d90af9d64efdfa73e7ba89730d9b5a5df3
180608ar <73707937+180608ar@users.noreply.github.com>
2021-02-23 01:31:10 -0800

    Add files via upload

--- Box.js
+++ Box.js
@@ -1,25 +1,6 @@
-class Box {
+class Box extends Baseclass {
     constructor(x,y){
-        var abc = {
-            restitution:1,
-            friction:1,
-            density:1
-        }
-        this.body= Bodies.rectangle(x,y,70,70,abc);
-        this.width = 70;
-        this.height = 70;
+        super(x,y,70,70);
         this.image = loadImage("sprites/wood1.png");
-        World.add(myWorld,this.body);
-    }
-
-    display(){
-        var pos = this.body.position;
-        var angle = this.body.angle;
-        push();
-        translate(pos.x,pos.y);
-        rotate(angle);
-        imageMode(CENTER);
-        image(this.image,0,0,this.width,this.height);
-        pop();
     }
 }
